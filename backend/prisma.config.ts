36a5390b09023103446a663da51a97e4dfdbe89a
Abdur Rauf Al Farras <alfarras74@gmail.com>
2026-07-12 16:00:23 +0700

    fix

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -16,6 +16,6 @@ export default defineConfig({
         path: 'prisma/migrations',
     },
     datasource: {
-        url: process.env.DIRECT_URL || 'mysql://dummy:dummy@localhost:3306/dummy',
+        url: process.env.DATABASE_URL || process.env.DIRECT_URL || 'mysql://dummy:dummy@localhost:3306/dummy',
     },
 });
